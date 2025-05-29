4afc7067f607d5e73ab070e5ee2d488162f8c369
Omkar2240 <omkarramgirwar123@gmail.com>
2025-05-29 18:21:54 +0530

    smooth scroll effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,35 @@
-
+"use client"
 import About from '@/components/main/About'
 import Contact from '@/components/main/Contact'
 import Hero from '@/components/main/Hero'
 import Project from '@/components/main/Project'
 import Skills from '@/components/main/Skills'
 import React from 'react'
+import { ReactLenis, useLenis } from 'lenis/react'
+
 
 const page = () => {
+  const lenis = useLenis((lenis) => {
+    // called every scroll
+    console.log(lenis)
+  })
   return (
+
     <main className='h-full w-full'>
-    <div className='flex flex-col h-[850px] gap-20 '>
-       <Hero />
-       <div > 
-        <About />
-       </div>
-       <Skills />
-       <div >
-         <Project />
-       </div>
-       <div id='contact'>
-        <Contact />
-       </div>
-    </div>
+      <ReactLenis root />
+      <div className='flex flex-col h-[850px] gap-20 '>
+        <Hero />
+        <div >
+          <About />
+        </div>
+        <Skills />
+        <div >
+          <Project />
+        </div>
+        <div id='contact'>
+          <Contact />
+        </div>
+      </div>
     </main>
   )
 }
